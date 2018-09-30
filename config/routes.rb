4d10589dbabe74ea43fe768e7c1636e34cf7b3da
RicardoSuarez <rsuarez.adv@gmail.com>
2018-09-30 18:16:43 -0300

    add Strong params

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        registration: 'users/registration'
+        registrations: 'users/registrations'
   }
   resources :histories
+  resources :users
 
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
